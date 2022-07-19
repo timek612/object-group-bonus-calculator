8d244eba4a850ec39db0b93de12d123d126d5a45
Taylor Moe <taylormoe93@gmail.com>
2022-07-19 16:06:36 -0500

    if else working tested

--- client.js
+++ client.js
@@ -42,39 +42,58 @@ const employees = [
 console.log( employees );
 
 
+
+// IF ELSE STATEMENTS
 function bonusCalculator (employees) { 
-  if (employees[i].reviewRating === 3) {
-    console.log('rating is 3');
-    let rating3 = 1.03; 
+  let totalBonus = 0;
+  
+  // LOOP
+  for (let i=0; i < employees.length; i++) {
+
+
+ 
+    if (employees[i].reviewRating === 3) {
+      console.log('rating is 3');
+      let rating3 = .03; 
+      totalBonus += rating3;
+    }
+    else if (employees[i].reviewRating === 4) {
+      console.log('rating is 4');
+      let rating4 = .04;
+      totalBonus += rating4;
   }
-  else if (employees[i].reviewRating === 4) {
-    console.log('rating is 4');
-    let rating4 = 1.04;
+    else if (employees[i].reviewRating >= 5) {
+      console.log('rating is 5');
+      let rating5 = .05;
+      totalBonus += rating5;
+    }
+    else {console.log('rating is 2 or below')}
+  
+
+  if (employees[i].employeeNumber.length === 4) { // possibly revisit
+    console.log('Employee number is 4 digits');
+    let numberLength = .05;
+    totalBonus += numberLength;
+  } 
+    else {console.log('employee number is not 4 digits')}
+
+  if (employees[i].annualSalary >= '65000') { // number is string
+    console.log('Annual salary is over 65000');
+    totalBonus -= .01;
+  } 
+    else {console.log('Salary is under 65000')}
+
+  if (totalBonus >= 0.13) {
+    console.log('bonus is set to 13%');
+    totalBonus = 0.13 
+  } 
+    else if (totalBonus > 0 && totalBonus < .12 ) {
+    console.log('bonus stays the same')
+    }
+    else {console.log('bonus is set to 0%')}
+    totalBonus += 1;
+    console.log(totalBonus);
+
+  }
 }
-  else if (employees[i].reviewRating >= 5) {
-    console.log('rating is 5');
-    let rating5 = 1.05;
-  }
-  else {console.log('rating is 2 or below')}
-}
-
-if (employees[i].employeeNumber.length === 4) { // possibly revisit
-  console.log('Employee number is 4 digits');
-  let numberLength = 1.05;
-} 
-  else {console.log('employee number is not 4 digits')}
-
-if (employees[i].annualSalary >= '65000') { // number is string
-  console.log('Annual salary is over 65000');
-  let salaryCap = 0.99;
-} 
-  else {console.log('Salary is under 65000')}
-
-if (totalBonus >= 1.13) {
-  console.log('bonus is set to 13%');
-  totalBonus = 1.13 
-} 
-  else if (totalBonus > 1 && totalBonus < 1.12 ) {
-   console.log('bonus stays the same')
-  }
-  else {console.log('bonus is set to 0%')}
+  bonusCalculator(employees);
